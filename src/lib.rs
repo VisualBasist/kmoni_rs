7a5d15bc1fd4bd5486778b619e12d9358dfcd997
ひよどりBasist <VisualBasist@users.noreply.github.com>
2024-01-03 19:48:52 +0900

    test: データの例をテストコードに移す

--- src/lib.rs
+++ src/lib.rs
@@ -54,64 +54,6 @@ struct LatestDataTimeResponse {
     result: ResultRawResponse,
 }
 
-/*
-{
-    "result": {
-        "status": "success",
-        "message": "データがありません",
-        "is_auth": true
-    },
-    "report_time": "",
-    "region_code": "",
-    "request_time": "20240102114930",
-    "region_name": "",
-    "longitude": "",
-    "is_cancel": "",
-    "depth": "",
-    "calcintensity": "",
-    "is_final": "",
-    "is_training": "",
-    "latitude": "",
-    "origin_time": "",
-    "security": {
-        "realm": "/webservice/hypo/eew/",
-        "hash": "5ca8b8104e01ceef0f061ad597606cbd87b492db"
-    },
-    "magunitude": "",
-    "report_num": "",
-    "request_hypo_type": "eew",
-    "report_id": ""
-}
-calcintensityは5弱とかにもなる
-{
-    "result": {
-        "status": "success",
-        "message": "",
-        "is_auth": true
-    },
-    "report_time": "2023/05/21 16:03:57",
-    "region_code": "",
-    "request_time": "20230521160357",
-    "region_name": "福島県沖",
-    "longitude": "141.5",
-    "is_cancel": false,
-    "depth": "20km",
-    "calcintensity": "2",
-    "is_final": false,
-    "is_training": false,
-    "latitude": "37.2",
-    "origin_time": "20230521160321",
-    "security": {
-        "realm": "/kyoshin_monitor/static/jsondata/eew_est/",
-        "hash": "b61e4d95a8c42e004665825c098a6de4"
-    },
-    "magunitude": "3.5",
-    "report_num": "2",
-    "request_hypo_type": "eew",
-    "report_id": "20230521160327",
-    "alertflg": "予報"
-}
- */
 #[derive(Deserialize, Debug)]
 pub struct EEW {
     result: ResultRawResponse,
@@ -126,6 +68,7 @@ pub struct EEW {
     is_cancel: bool,
     // TODO: depth: f64,
     depth: String,
+    // TODO: calcintensityは5弱とかにもなる
     calcintensity: String,
     is_final: bool,
     is_training: bool,
@@ -206,6 +149,40 @@ mod tests {
                 "report_id": "20230521160327",
                 "alertflg": "予報"
             }"#,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn nodata() {
+        serde_json::from_str::<EEW>(
+            r#"{
+    "result": {
+        "status": "success",
+        "message": "データがありません",
+        "is_auth": true
+    },
+    "report_time": "",
+    "region_code": "",
+    "request_time": "20240102114930",
+    "region_name": "",
+    "longitude": "",
+    "is_cancel": "",
+    "depth": "",
+    "calcintensity": "",
+    "is_final": "",
+    "is_training": "",
+    "latitude": "",
+    "origin_time": "",
+    "security": {
+        "realm": "/webservice/hypo/eew/",
+        "hash": "5ca8b8104e01ceef0f061ad597606cbd87b492db"
+    },
+    "magunitude": "",
+    "report_num": "",
+    "request_hypo_type": "eew",
+    "report_id": ""
+}"#,
         )
         .unwrap();
     }
